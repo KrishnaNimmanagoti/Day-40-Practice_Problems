7b22624e172a54f7577c5437c5b4817f9b55444e
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-08 17:17:05 +0530

    Functions Practice Problems

---
+++ functionsProblems/checkPalindrome.js
@@ -0,0 +1,19 @@
+function checkPalindrome(number) {
+    const temp = number;
+    var reverse = 0;
+    var sum = 0;
+    while (number > 0) {
+        sum = number % 10;
+        number = parseInt(number / 10);
+        reverse = reverse * 10 + sum;
+    }
+    if (temp == reverse) {
+        console.log(reverse + " is palindrome");
+    } else {
+        console.log(reverse + " is not palindrome");
+    }
+}
+
+var digit = require("readline-sync");
+var number = parseInt(digit.question("Enter Number To Check Palindrome Or Not "))
+checkPalindrome(number);
